93bd71293f1505261aa084103e4f9c0101b5e58b
Snehallohar123 <143900288+Snehallohar123@users.noreply.github.com>
2024-11-25 07:07:03 +0530

    Update new_ex9.java

--- new_ex9.java
+++ new_ex9.java
@@ -1,48 +1,45 @@
-import java.util.*;
-import java.io;
-class createfile
-{
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the file name");
-		String filename=sc.nextLine();
-		writefile(filename,sc);
-		read_file(filename,sc);	
-	}	
-		
-	public void writefile(String filename,Scanner sc)
-	{	try(
-		FileWrite writer=new FileWrite(filename))
-		{
-		String content=sc.nextline();
-		writer.write(content +"\n");
-		System.out.println("content write successfully!!");
-		}
-	
-		catch(IOException e)
-		{
-		System.out.println("error occur"+ e.getmessage());
-		}
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
-	}	
-	
+class Readwrite {
+    public static void main(String[] args) {
+        String filename;
+        Scanner sc = new Scanner(System.in);
 
-	public void read_file(String filename,Scanner sc)
-	{
-		try(BufferedReader reader=new BufferedReader(new Filereader(filename)))
-		{
-			String content;
-			while((content=reader.readLine()) != null)
-			{
-				System.out.print(content);
-			}
-		}
-		catch(IOException e)
-	{
-		System.out.println("error occur"+ e.getmessage());
-	}
+        System.out.println("Enter the filename:");
+        filename = sc.nextLine();
 
-	}	
-		
+        // Reading the file
+        try (FileInputStream fstream = new FileInputStream(filename)) {
+            int i;
+            System.out.println("File content:");
+            while ((i = fstream.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
+
+        // Asking if the user wants to add new content
+        System.out.println("\nDo you want to add new content to the file? (yes/no)");
+        String response = sc.nextLine();
+
+        if (response.equalsIgnoreCase("yes")) {
+            System.out.println("Add some content:");
+            String content = sc.nextLine();
+
+            // Appending content to the file
+            try (FileOutputStream fostream = new FileOutputStream(filename, true)) {
+                byte[] bytes = content.getBytes();
+                fostream.write(bytes);
+                System.out.println("The content was added successfully.");
+            } catch (IOException e) {
+                System.out.println("Error adding content: " + e.getMessage());
+            }
+        }
+
+        sc.close();
+    }
 }
